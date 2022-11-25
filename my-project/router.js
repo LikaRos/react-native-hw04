9d6710da8394673d5d5e3fed09002c2da30ade5b
Lika <lika.rossinskaya@gmal.com>
2022-11-25 09:58:33 +0200

    router updated

--- my-project/router.js
+++ my-project/router.js
@@ -10,6 +10,7 @@ import { LoginScreen } from "./Screens/Auth/LoginScreen";
 import { RegistrationScreen } from "./Screens/Auth/RegistrationScreen";
 import CreatePostsScreen from "./Screens/MainScreen/CreatePostsScreen";
 import MapScreen from "./Screens/MainScreen/MapScreen";
+import HomeScreen from "./Screens/MainScreen/HomeScreen";
 import PostsScreen from "./Screens/MainScreen/PostsScreen";
 import ProfileScreen from "./Screens/MainScreen/ProfileScreen";
 /*Icons*/
@@ -44,16 +45,6 @@ export const useRoute = (isAuth) => {
         options={{
           headerShown: false,
           tabBarIcon: ({ focused, size, color }) => (
-            <AntDesign name="plus" size={size} color={color} />
-          ),
-        }}
-        name="Posts"
-        component={PostsScreen}
-      ></MainTab.Screen>
-      <MainTab.Screen
-        options={{
-          headerShown: false,
-          tabBarIcon: ({ focused, size, color }) => (
             <Entypo name="grid" size={size} color={color} />
           ),
         }}
@@ -63,10 +54,20 @@ export const useRoute = (isAuth) => {
       <MainTab.Screen
         options={{
           headerShown: false,
+          tabBarIcon: ({ focused, size, color }) => (
+            <AntDesign name="plus" size={size} color={color} />
+          ),
+        }}
+        name="Posts"
+        component={PostsScreen}
+      ></MainTab.Screen>
+      {/* <MainTab.Screen
+        options={{
+          headerShown: false,
         }}
         name="Map"
         component={MapScreen}
-      ></MainTab.Screen>
+      ></MainTab.Screen> */}
       <MainTab.Screen
         options={{
           headerShown: false,
